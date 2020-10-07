89c7e3b5c1d31b0bb6bac7a9acaa75425cdb6e0b
Ismael <ismaeljesusquiros@gmail.com>
2020-10-07 22:22:47 +0200

    Base hecha
    Queda pulir código

--- app/src/main/java/es/iessaladillo/pedrojoya/pr02_greetimproved/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr02_greetimproved/MainActivity.java
@@ -1,8 +1,12 @@
 package es.iessaladillo.pedrojoya.pr02_greetimproved;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.FocusFinder;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,37 +16,39 @@ public class MainActivity extends AppCompatActivity {
 
     private MainActivityBinding binding;
     private boolean premium;
-    private String pronoun = "Mr. ";
+    private String pronoun = "Mr.";
     private boolean polite;
     private int contador = 0;
+    private int charName = 0;
+    private int charSurname = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = MainActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.lblNameChar.setTextColor(getResources().getColor(R.color.colorAccent));
         image();
         isPremium();
         isPolite();
         show();
+        showNameChar();
+        showSurnameChar();
         contador();
+        writeNameSurname();
     }
 
     private void isPremium(){
-        binding.swtPremium.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(((CompoundButton)view).isChecked()){
-                    premium=true;
-                    visibility(8);
-                    contador = 0;
-                }
-                else{
-                    premium=false;
-                    visibility(0);
-                    contador = 0;
-                }
+        binding.swtPremium.setOnClickListener(view -> {
+            if(((CompoundButton)view).isChecked()){
+                premium=true;
+                visibility(8);
             }
+            else{
+                premium=false;
+                visibility(0);
+            }
+            contador = 0;
         });
     }
 
@@ -58,48 +64,108 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void isPolite(){
-        binding.chkPolitely.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(((CompoundButton)view).isChecked()){
-                    polite=true;
-                }
-                else{
-                    polite=false;
-                }
+        binding.chkPolitely.setOnClickListener(view -> {
+            if(((CompoundButton)view).isChecked()){
+                polite=true;
+            }
+            else{
+                polite=false;
             }
         });
 
     }
 
+    private boolean nameEmpty(){
+        String name = binding.txtName.getText().toString();
+        return name.isEmpty();
+    }
+
+    private boolean surnameEmpty(){
+        String surname = binding.txtSurname.getText().toString();
+        return surname.isEmpty();
+    }
+
     private void show(){
-        binding.btnGreet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = binding.txtName.getText().toString();
-                String surname = binding.txtSurname.getText().toString();
-                if(!name.isEmpty() && !surname.isEmpty()){
-                    if(contador == 10){
-                        binding.lblMessage.setText("Buy premium subscription to go on greeting!!");
-                    }
-                    else {
-                        if (polite) {
-                            binding.lblMessage.setText("Good morning " + pronoun + name + " " + surname + ". Pleased to meet you");
-                            if(!premium){
-                                sumarContador();
-                            }
-                            contador();
-                        } else {
-                            binding.lblMessage.setText("Hello " + name + " " + surname + ". What's up?");
-                            if(!premium){
-                                sumarContador();
-                            }
-                            contador();
+        binding.btnGreet.setOnClickListener(view -> {
+            if(!nameEmpty() && !surnameEmpty()){
+                if(contador == 10){
+                    Toast.makeText(this, getString(R.string.premiunMessage), Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (polite) {
+                        Toast.makeText(this, getString(R.string.politeMessage, pronoun, binding.txtName.getText().toString(), binding.txtSurname.getText().toString()), Toast.LENGTH_SHORT).show();
+                        if(!premium){
+                            sumarContador();
                         }
+                        contador();
+                    } else {
+                        Toast.makeText(this, getString(R.string.notPoliteMessage, binding.txtName.getText().toString(), binding.txtSurname.getText().toString()), Toast.LENGTH_SHORT).show();
+                        if(!premium){
+                            sumarContador();
+                        }
+                        contador();
                     }
                 }
             }
+            else{
+                if (nameEmpty()){
+                    binding.txtName.setError(getString(R.string.errorMessage));
+
+                }
+                else{
+                    binding.txtSurname.setError(getString(R.string.errorMessage));
+                }
+            }
         });
+    }
+
+    private void writeNameSurname(){
+        binding.txtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                showNameChar();
+            }
+        });
+
+        binding.txtSurname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                showSurnameChar();
+            }
+        });
+
+        binding.txtName.setOnFocusChangeListener((view, hasFocus) -> {
+            if (hasFocus) {
+                binding.lblNameChar.setTextColor(getResources().getColor(R.color.colorAccent));
+            } else {
+                binding.lblNameChar.setTextColor(getResources().getColor(R.color.textPrimary));
+            }
+        });
+
+        binding.txtSurname.setOnFocusChangeListener((view, hasFocus) -> {
+            if (hasFocus) {
+                binding.lblSurnameChar.setTextColor(getResources().getColor(R.color.colorAccent));
+            } else {
+                binding.lblSurnameChar.setTextColor(getResources().getColor(R.color.textPrimary));
+            }
+        });
+
     }
 
     private void sumarContador(){
@@ -110,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void contador(){
         binding.pgbCounter.setProgress(contador);
-        binding.lblCounter.setText(contador + " of " + binding.pgbCounter.getMax());
+        binding.lblCounter.setText(getString(R.string.contador ,contador, binding.pgbCounter.getMax()));
     }
 
     private void image(){
@@ -118,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 binding.imgTreatment.setImageResource(R.drawable.ic_mr);
-                pronoun = "Mr. ";
+                pronoun = "Mr.";
             }
         });
 
@@ -126,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 binding.imgTreatment.setImageResource(R.drawable.ic_mrs);
-                pronoun = "Mrs. ";
+                pronoun = "Mrs.";
             }
         });
 
@@ -134,10 +200,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 binding.imgTreatment.setImageResource(R.drawable.ic_ms);
-                pronoun = "Ms. ";
+                pronoun = "Ms.";
             }
         });
 
+    }
+
+    private void showNameChar(){
+        charName = binding.txtName.getText().length();
+        binding.lblNameChar.setText(getResources().getQuantityString(
+                R.plurals.charNumber,(getResources().getInteger(R.integer.charLimit)-charName),
+                (getResources().getInteger(R.integer.charLimit)-charName)));
+    }
+
+    private void showSurnameChar(){
+        charSurname = binding.txtSurname.getText().length();
+        binding.lblSurnameChar.setText(getResources().getQuantityString(
+                R.plurals.charNumber,(getResources().getInteger(R.integer.charLimit)-charSurname),
+                (getResources().getInteger(R.integer.charLimit)-charSurname)));
     }
 
 }
